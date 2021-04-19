0d7a13a634c95967b2ef4d26dd74335a39003d3e
Clivern <hello@clivern.com>
2021-04-19 22:49:48 +0200

    update

--- src/main/java/com/clivern/asynq/broker/MessageBrokerInterface.java
+++ src/main/java/com/clivern/asynq/broker/MessageBrokerInterface.java
@@ -13,5 +13,51 @@
  */
 package com.clivern.asynq.broker;
 
+import java.util.HashMap;
+
 /** MessageBrokerInterface Interface */
-public interface MessageBrokerInterface {}
+public interface MessageBrokerInterface {
+
+    /**
+     * Configure the broker
+     *
+     * @param configs a hashmap of configs
+     */
+    public void configure(HashMap<String, String> configs);
+
+    /**
+     * Connect to the broker
+     *
+     * @return whether it connected or not
+     */
+    public Boolean connect();
+
+    /**
+     * Disconnect from the broker
+     *
+     * @return whether it disconnected or not
+     */
+    public Boolean disconnect();
+
+    /**
+     * Reconnect to the broker
+     *
+     * @return whether it reconnected or not
+     */
+    public Boolean reconnect();
+
+    /**
+     * Consume the Queue
+     *
+     * @param consumer the consumer instance
+     */
+    public void consume(ConsumerInterface consumer);
+
+    /**
+     * Send a Message
+     *
+     * @param message the message
+     * @return whether sending succeeded or not
+     */
+    public Boolean send(MessageInterface message);
+}
